b1467afd8ab0a8bbf70e6a89d16f3679d4f9b776
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-11 11:24:29 +0300

    implement error messages with flash

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,6 +30,10 @@ router.get('/new', (req, res) => {
 
 router.get('/:id',catchAsync(async(req,res) =>{
     const campground =  await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error', 'campground not found!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', {campground})
 }))
 
@@ -42,6 +46,10 @@ router.post('/',validateCampground, catchAsync(async(req, res) => {
 
 router.get('/:id/edit', catchAsync(async(req,res) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+        req.flash('error', 'campground not found!');
+        return res.redirect('/campgrounds')
+    } 
     res.render('campgrounds/edit', {campground})
 }))
 
