59beb567c61c38008386e9b0261312ae068553b9
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-13 12:56:41 +0300

    create user registeration logic

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    // model
+    User = require('../models/user'),
+    // error handlers
+    catchAsync = require('../utils/catchAsync')
 // 
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
-router.post('/register', (req, res) => {
-    res.send('register post route works')
-})
+router.post('/register', catchAsync(async(req, res, next) => {
+    const {username, email} = req.body;
+    const user = await User.register(new User({username, email}), req.body.password);
+    req.login(user, (err) => {
+        if(err) return next(err);
+        req.flash('success', 'Welcome to yelpcamp')
+        res.redirect('/campgrounds')
+    })
+}))
 //
 router.get('/login', (req, res) => {
     res.render('users/login')
