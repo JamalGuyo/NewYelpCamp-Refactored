ab3f542928c9b566d9786e63eb4f83f6bf42665e
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-14 10:26:15 +0300

    create author in campground schema and seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5ffec397a5f89240a4e65dd5',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
