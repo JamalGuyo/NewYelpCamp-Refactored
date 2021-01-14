8ddb2e46803709483a02c6c2594bf686710a2ce5
Jamal Guyo <jamalguyo08@gmail.com>
2021-01-14 10:15:22 +0300

    create a return to previous page feature after login

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()){
         return next();
     }
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You need to be logged in');
     res.redirect('/login');
 }
